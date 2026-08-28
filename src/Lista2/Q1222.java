d960ba2b69476b3c98585e56a4669ebf5eb1a227
Victor Targino <victor.melentovytch@dataprev.gov.br>
2026-08-28 12:23:54 -0300

    Lista 2 completa

--- src/Lista2/Q1222.java
+++ src/Lista2/Q1222.java
@@ -1,6 +1,38 @@
 package Lista2;
 
+
+import java.util.Scanner;
+
 public class Q1222 {
 
-    public static void main (String[] args){}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (scanner.hasNext()) {
+
+            int n = scanner.nextInt();
+            int l = scanner.nextInt(); // Lines per Page
+            int c = scanner.nextInt(); //Character per line
+
+            int linhas = 1;
+            int atual = 0;
+
+            for (int i = 0; i < n; i++) {
+                String palavra = scanner.next();
+
+                if (atual == 0) {
+                    atual = palavra.length();
+                } else if (atual + 1 + palavra.length() <= c) {
+                    atual += 1 + palavra.length();
+                } else {
+                    linhas++;
+                    atual = palavra.length();
+                }
+            }
+
+            System.out.println((linhas + l - 1) / l);
+        }
+    }
 }
